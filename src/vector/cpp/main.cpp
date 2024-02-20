5fc9b4200b7f267a37d7d14a1331b42c3336ac14
osip123 <131646261+osip123@users.noreply.github.com>
2024-02-20 21:37:28 +0300

    vecror3 ready

--- src/vector/cpp/main.cpp
+++ src/vector/cpp/main.cpp
@@ -9,6 +9,14 @@ int main() {
 	y = 45.7;
 	z = 42.6;
 
+	vector.gorRpos = true;
+	vector.repos = false;
+	vector.repos = true;
+
+	vector.speed = 12.4;
+	vector.vertPepos = 0;
+	vector.gorspeed = 0.6f;
+
 	vector.calcVector3(x, y ,z);
 	return 0;
 }
--- src/vector/cpp/vector3.cpp
+++ src/vector/cpp/vector3.cpp
@@ -4,30 +4,78 @@
 class Vector3
 {
 public:
-	float x, y, z;
+	bool vertPepos, gorRpos, repos;
+	float speed, vertspeed, gorspeed;
 
 	float calcVector3(float x, float y, float z) {
-		float res;
-		res = x + y + z;
-		std::cout << res;
-		return res;
+
+
+		if (repos)
+		{
+			x = calcX(x, speed);
+		}
+		
+		else if(vertPepos)
+		{
+			z = calcZ(z, vertspeed);
+		}
+
+		else
+		{
+			y = calcY(y, gorspeed);
+		}
+
+		std::cout << x, y, z;
+
+		return x, y, z;
 	}
 
 	
 private:
 	double calcX(float x, float speed) {
 		double res;
+		if (speed > 0)
+		{
+			res = x + speed;
+		}
+		else
+		{
+			res = x - speed;
+		}
 		return res;
 	}
 
 	double calcZ(float z, float vertspeed) {
 		double res;
+		if (z >= 0)
+		{
+			if (vertspeed > 0)
+			{
+				res = z + vertspeed;
+			}
+			else {
+				res = z - vertspeed;
+			}
+		}
+
+		else
+		{
+			res = 0;
+		}
+
 		return res;
 	}
 
 	double calcY(float y, float gorspeed) {
 		double res;
+		if (gorspeed > 0)
+		{
+			res = y + gorspeed;
+		}
+		else
+		{
+			res = y - gorspeed;
+		}
 		return res;
 	}
 };
-
